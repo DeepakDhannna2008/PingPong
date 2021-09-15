556a25fc5bcaae12a7e1920395ee00d628d653d1
DeepakDhannna2008 <90776787+DeepakDhannna2008@users.noreply.github.com>
2021-09-15 20:44:47 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,134 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["11d5d9f0-6932-45de-ba25-6f4ac37fe9a9","7d58941f-a7d8-42da-85c6-3efa6131461a","42b7aa29-c5f8-44fe-9d49-8540deca9c13","e92d7a76-cb9d-426f-90f2-1f5356f539aa","9cbbc122-d7a3-49de-bfdd-391aa41fbd03","c3713c95-d1ca-4947-8529-79c487b326f9"],"propsByKey":{"11d5d9f0-6932-45de-ba25-6f4ac37fe9a9":{"name":"robot","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/11d5d9f0-6932-45de-ba25-6f4ac37fe9a9.png","frameSize":{"x":77,"y":69},"frameCount":1,"looping":true,"frameDelay":4,"version":"oV1YJjscPbSLXKlw_RRfNFJwPykhG5Q6","loadedFromSource":true,"saved":true,"sourceSize":{"x":77,"y":69},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/11d5d9f0-6932-45de-ba25-6f4ac37fe9a9.png"},"7d58941f-a7d8-42da-85c6-3efa6131461a":{"name":"player","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/7d58941f-a7d8-42da-85c6-3efa6131461a.png","frameSize":{"x":60,"y":91},"frameCount":1,"looping":true,"frameDelay":4,"version":"Av88Yv4jNwgQ7taGfTGbmkZ2VT98n_JW","loadedFromSource":true,"saved":true,"sourceSize":{"x":60,"y":91},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/7d58941f-a7d8-42da-85c6-3efa6131461a.png"},"42b7aa29-c5f8-44fe-9d49-8540deca9c13":{"name":"player_kick","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/42b7aa29-c5f8-44fe-9d49-8540deca9c13.png","frameSize":{"x":77,"y":77},"frameCount":1,"looping":true,"frameDelay":4,"version":"wl3UmnqnNRf8xAOI84v7GW3X9urrRI3R","loadedFromSource":true,"saved":true,"sourceSize":{"x":77,"y":77},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/42b7aa29-c5f8-44fe-9d49-8540deca9c13.png"},"e92d7a76-cb9d-426f-90f2-1f5356f539aa":{"name":"player_fall","sourceUrl":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/e92d7a76-cb9d-426f-90f2-1f5356f539aa.png","frameSize":{"x":92,"y":51},"frameCount":1,"looping":true,"frameDelay":4,"version":"RtL1fLlEr3UUfVdWxHPunIulfUhlTvm2","loadedFromSource":true,"saved":true,"sourceSize":{"x":92,"y":51},"rootRelativePath":"assets/v3/animations/-avJdgAUNRjyTsnOFCs8c0UqlTsKHiEVHv4fvZZZiKA/e92d7a76-cb9d-426f-90f2-1f5356f539aa.png"},"9cbbc122-d7a3-49de-bfdd-391aa41fbd03":{"name":"ball","sourceUrl":"assets/api/v1/animation-library/gamelab/KAKckB.0WJDP55kNGzIZIfW5wf7Rk5mG/category_sports/soccer_bw.png","frameSize":{"x":393,"y":394},"frameCount":1,"looping":true,"frameDelay":2,"version":"KAKckB.0WJDP55kNGzIZIfW5wf7Rk5mG","categories":["sports"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":393,"y":394},"rootRelativePath":"assets/api/v1/animation-library/gamelab/KAKckB.0WJDP55kNGzIZIfW5wf7Rk5mG/category_sports/soccer_bw.png"},"c3713c95-d1ca-4947-8529-79c487b326f9":{"name":"back","sourceUrl":"assets/api/v1/animation-library/gamelab/J6AoCEQiDKlKsdVff_tiZnDVueArBO2V/category_backgrounds/soccer_field.png","frameSize":{"x":400,"y":400},"frameCount":1,"looping":true,"frameDelay":2,"version":"J6AoCEQiDKlKsdVff_tiZnDVueArBO2V","categories":["backgrounds"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":400,"y":400},"rootRelativePath":"assets/api/v1/animation-library/gamelab/J6AoCEQiDKlKsdVff_tiZnDVueArBO2V/category_backgrounds/soccer_field.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+ var bg = createSprite(200,200, 400, 400)
+    bg.shapeColor="white"
+var playerPaddle= createSprite(390,200,10,100);
+    playerPaddle.shapeColor="blue";
+    playerPaddle.setAnimation("player")
+    var computerPaddle= createSprite(30,200,10,100);
+    computerPaddle.shapeColor="red";
+    computerPaddle.setAnimation("robot")
+    var ball= createSprite(200,200,10,10);
+    ball.shapeColor="yellow";
+    ball.setAnimation("ball");
+    ball.scale = 0.1;
+    bg.setAnimation("back")
+
+function draw() {
+  background("white");
+  
+  if(ball.isTouching(playerPaddle) || ball.isTouching(computerPaddle))
+  {
+    playSound("assets/hit.mp3");
+  }
+ 
+  
+if(ball.isTouching(playerPaddle))
+  {
+   playerPaddle.setAnimation("player_kick") 
+  }  
+  if (ball.isTouching(computerPaddle))
+  {
+    playerPaddle.setAnimation("player")
+  }
+  
+  
+  
+  if (keyDown("up")) {
+    playerPaddle.y=playerPaddle.y-10;
+  }
+  
+  if (keyDown("down")) {
+    playerPaddle.y=playerPaddle.y+10;
+  }
+  
+  if(keyDown("space"))
+  {
+     ball.velocityX=2;
+     ball.velocityY=3;
+  }
+  
+  computerPaddle.y=ball.y;
+
+  drawnet();
+  
+ 
+    
+
+  createEdgeSprites();
+  
+  if (ball.isTouching(topEdge)||ball.isTouching(bottomEdge) ) {
+    playSound("assets/category_hits/puzzle_game_button_01.mp3")
+  }
+  ball.bounceOff(topEdge);
+  ball.bounceOff(bottomEdge);
+  ball.bounceOff(computerPaddle);
+  ball.bounceOff(playerPaddle);
+  playerPaddle.bounce(edges)
+  drawSprites();
+  
+}
+
+
+
+function drawnet()
+{  
+  for(var num=0;num<400;num=num+20)
+  {
+    line(200,num,200,num+10);
+  }
+}
+
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
